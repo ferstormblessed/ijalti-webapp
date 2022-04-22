6fbbb448e4831d89b66d66f64a6b11b5f363d268
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-04-21 21:53:55 -0500

    colorPalette added to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: "#61838D",
+      secondary: "#BEDCDA",
+      primaryButton: "#A2ADC3",
+      secondaryButton: "#E6E5ED",
+      background: "#F9F8FE",
+    },
     extend: {},
   },
   plugins: [],
