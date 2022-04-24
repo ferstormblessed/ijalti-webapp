631ad64fc6fe7a8525e37d21bb0e9097f52b8174
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-04-24 15:09:25 -0500

    working taiwind and component import

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
-import Form from "../components/Form.tsx";
+import Form from "../components/form.tsx";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-10">
       <Form/>
     </div>
   );
