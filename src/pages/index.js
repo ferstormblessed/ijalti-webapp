f866c0bb3240be08c31535fd204dc1aa3bdcb5ac
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-04-24 14:36:26 -0500

    push antes de arreglar tailwind

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import Form from "../components/Form.tsx";
+
+export default function Home() {
+  return (
+    <div>
+      <Form/>
+    </div>
+  );
+}
