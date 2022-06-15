0fc0b111aa5b85398ac0d07f4da2cf59bd73905c
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-06-15 14:04:32 -0500

    removed profile

--- pages/dashboardEmpresa/index.tsx
+++ pages/dashboardEmpresa/index.tsx
@@ -1,16 +1,14 @@
 import Dashboard from "../../components/dashboard";
-import DashboardEmpresa from "../../components/dashboardEmpresa"
-import ProfileUser from "../../components/profile";
+import DashboardEmpresa from "../../components/dashboardEmpresa";
 import EmpresaForm from "../../components/empresaForm";
-import CorpForm from "../../components/corpForm"
+import CorpForm from "../../components/corpForm";
 import LoginEmpresaScreen from "../../components/layouts/loginEmpresa";
 const LoginEmpresa = () => {
   return (
     <div>
-     <DashboardEmpresa>
-        <CorpForm/>
+      <DashboardEmpresa>
+        <CorpForm />
       </DashboardEmpresa>
-        
     </div>
   );
 };
